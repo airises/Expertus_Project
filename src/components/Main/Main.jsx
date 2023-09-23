e203b34a572998fa29728a3fae6e04b2805f761c
nursiDevv <nursi.devv@gmail.com>
2023-09-23 19:49:07 +0600

    main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import { ArraiMain } from './Arr';
+import React from "react";
+import { styled } from "@mui/material";
+import { ArraiMain } from "./Arr";
+
 const Main = () => {
-    return (
-        <div>
-            {
-                ArraiMain.map((el)=>{
-                    return(
-                        <div>
-                            <h1>{el.text}</h1>
-                            <div>
-                                <img src={el.img} alt="" />
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    );
+  return (
+    <div>
+      {ArraiMain.map((el, index) => (
+        <Container key={index}>
+          <h1>{el.text}</h1>
+          <div>
+            <Img src={el.img} alt="" />
+          </div>
+        </Container>
+      ))}
+    </div>
+  );
 };
+
+const Container = styled("div")``;
+const Img = styled("img")`
+  width: 200px;
+  height: 200px;
+  border-radius: 10px;
+`;
 
 export default Main;
