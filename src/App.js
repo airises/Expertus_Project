469232039b5a5cce1776bdb7afd11695625cbd51
Altynai-Dev <altynai.imnl@gmail.com>
2023-09-23 15:56:15 -0400

    login and regiater layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./components/pages/Login/Login";
-import Register from "./components/pages/Register/Register";
+
 
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      
     </div>
   );
 }
