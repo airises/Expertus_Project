f94916ae9296142a8f545ca73fd72cf2c14c03c8
nursiDevv <nursi.devv@gmail.com>
2023-09-23 20:13:35 +0600

    .

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { store } from "./store";
 import RouterProvider from "./routes/AppProvider";
-import Main from "./components/Main/Main";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-      <Main/>
         <RouterProvider />
       </BrowserRouter>
     </Provider>
