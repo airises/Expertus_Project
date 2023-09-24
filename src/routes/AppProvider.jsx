1f81bc783833dcb546afbce05f374709b71f7e51
Altynai-Dev <altynai.imnl@gmail.com>
2023-09-23 23:57:46 -0400

    crud register

--- src/routes/AppProvider.jsx
+++ src/routes/AppProvider.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "../components/pages/Login/Login";
 import Register from "../components/pages/Register/Register";
-
+import Home from "../components/pages/Home/Home";
 function RouterProvider() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
